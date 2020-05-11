e81cf15e73dc8773a5fcf3a8e086957b2b004315
NataliaRydza <natalie.rydza@gmail.com>
2020-05-11 20:45:15 +0200

    Show list from array

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 'use strict';
 
 console.log('funguju!');
+
+const tasks = ['Vyprat ponožky', 'Naučit se funkce vyššího řádu', 'Nakoupit na víkend'];
+
+const updateTasks = () => {
+  const todoElm = document.querySelector('.todo__tasks');
+  todoElm.innerHTML = ``;
+  for (let i = 0; i < tasks.length; i++) {
+    todoElm.innerHTML += `<div class="task">${tasks[i]}</div>`
+  }
+}
+
+updateTasks()
